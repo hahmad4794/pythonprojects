b7ff4d06a672b63013fdc71dce4ba5453f9072db
root <root@devx-python001.ziyotek.local>
2020-03-30 21:38:34 -0400

    added new changes

---
+++ game.py
@@ -0,0 +1,9 @@
+# Author: Hassan Ahmad
+# Contact: hassanghaffar79@gmail.com
+# A fun little python game
+
+name= input("What is your name?")
+age= input("What is your age?")
+
+print("Hi," + name + "!")
+print("Your age is " + str(age))
